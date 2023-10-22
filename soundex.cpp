1696de09e57ae62b318bbda4ee11299228dce087
directpi <directpiter1@yandex.ru>
2023-10-22 23:34:18 +0300

    soundex.cpp done

--- soundex.cpp
+++ soundex.cpp
@@ -1,36 +1,51 @@
 #include <iostream>
 #include <string>
 int main() {
-    std::string str1, str2;
-    // int count = 0;
+    std::string str1;
     std::getline(std::cin, str1);
-    // std::getline(std::cin, str2);
+    // if (str1.size() > 20) {
+    //     exit(1);
+    // }
     for (size_t i = 0; i != str1.size(); ++i) {
         char c = str1[i];
         if (i != 0 &&
             (c == 'a' || c == 'e' || c == 'h' || c == 'i' || c == 'o' || c == 'u' || c == 'w' || c == 'y')) {
-            std::cout << i << "\t" << str1[i] << "\t";
             str1.erase(i, 1);
-            //     ++d;
-            // } else {
-            //     d = 0;
-            // }
-            // if (d > 0) {
-            //     --count;
-            // } else {
-            //     ++count;
+            --i;
         }
-        std::cout << i << "_\n";
     }
-    std::cout << "\n";
-    // count = 0;
-    // for (char c : str2) {
-    //     if (count != 0 &&
-    //         (c == 'a' || c == 'e' || c == 'h' || c == 'i' || c == 'o' || c == 'u' || c == 'w' || c == 'y'))
-    //         { str2.erase(count, 1); std::cout << count << "\t";
-    //         ++count;
-    //     }
-    // }
-    std::cout << str1 << "\n";
-    // std::cout << str2 << "\n";
+    // std::cout << str1 << " -> ";
+    for (size_t i = 0; i < str1.size(); ++i) {
+        char c = str1[i];
+        if (i != 0 && (c == 'b' || c == 'f' || c == 'p' || c == 'v')) {
+            str1.replace(i, 1, "1");
+        } else if (i != 0 && (c == 'c' || c == 'g' || c == 'j' || c == 'k' || c == 'q' || c == 's' ||
+                              c == 'x' || c == 'z')) {
+            str1.replace(i, 1, "2");
+        } else if (i != 0 && (c == 'd' || c == 't')) {
+            str1.replace(i, 1, "3");
+        } else if (i != 0 && (c == 'l')) {
+            str1.replace(i, 1, "4");
+        } else if (i != 0 && (c == 'm' || c == 'n')) {
+            str1.replace(i, 1, "5");
+        } else if (i != 0 && (c == 'r')) {
+            str1.replace(i, 1, "6");
+        }
+    }
+    // std::cout << str1 << " -> ";
+    for (size_t i = 0; i < str1.size() - 1; ++i) {
+        if (str1[i] == str1[i + 1]) {
+            str1.erase(i + 1, 1);
+            i--;
+        }
+    }
+    // std::cout << str1 << " -> ";
+    if (str1.size() < 4) {
+        for (size_t i = str1.size(); i < 4; ++i) {
+            str1.insert(i, "0");
+        }
+    } else {
+        str1.resize(4);
+    }
+    std::cout << str1;
 }
