4f021540733843e3484031f668de8c4a65d1079b
directpi <directpiter1@yandex.ru>
2023-10-31 21:57:32 +0300

    Ya ex2 done

--- ex2.cpp
+++ ex2.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
-uint64_t znam(uint64_t b, uint64_t d) {
-    uint64_t res = 0, tmp_b = 0, tmp_d = 0;
+
+long long znam(long long b, long long d) {
+    long long res = 0;
     if (b == d) {
         res = b;
     } else {
         int i = 2;
-        tmp_b = b;
-        tmp_d = d;
+        long long tmp_b = b;
+        long long tmp_d = d;
         while (tmp_b != tmp_d) {
-            if (tmp_b < tmp_d) {
-                tmp_b = b * i;
-                if ((tmp_b % d) == b) {
-                    res = tmp_b;
-                    tmp_d = tmp_b;
-                }
-            } else {
-                tmp_d = d * i;
-                if ((tmp_d % b) == d) {
-                    res = tmp_d;
-                    tmp_b = tmp_d;
-                }
+            tmp_b = b * i;
+            if ((tmp_b % d) == 0 || (tmp_b == d)) {
+                res = tmp_b;
+                tmp_d = tmp_b;
             }
             ++i;
         }
@@ -28,28 +21,38 @@ uint64_t znam(uint64_t b, uint64_t d) {
 }
 
 int main() {
-    uint64_t a, b, c, d, m, n;
+    long long a, b, c, d;
 
     std::cin >> a >> b >> c >> d;
-    if (b != 0 && d != 0) {
-        n = znam(b, d);
-
-        if (n == b) {
+    if (b != 0 && d != 0 && a <= 100 && b <= 100 && c <= 100 && d <= 100) {
+        long long n = znam(b, d);
+        long long m = 0;
+        if (n == b && d == n) {
             m = a + c;
         } else {
-            m = (a * n / b) / n + (c * n / d) / n;
+            m = (a * n / b) + (c * n / d);
         }
-        std::cout << m << " " << n << "\n";
         if (m != n) {
             while (n % m == 0 && m != 1) {
                 n = n / m;
                 m = m / m;
             }
-            std::cout << m << " " << n;
-
+            for (int j = 100; j > 1; --j) {
+                while (n % j == 0 && m % j == 0) {
+                    n = n / j;
+                    m = m / j;
+                }
+            }
+            std::cout << m << " " << n << "\n";
         } else {
             m = m / m;
             n = n / n;
+            for (int j = 100; j > 1; --j) {
+                while (n % j == 0 && m % j == 0) {
+                    n = n / j;
+                    m = m / j;
+                }
+            }
             std::cout << m << " " << n;
         }
     } else {
