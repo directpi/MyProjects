f4e4ebb7301d605bc6386742bd8b967637cd8381
directpi <directpiter1@yandex.ru>
2023-11-02 22:51:05 +0300

    Ya partition TE

---
+++ partition2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+void q_sort(std::vector<int>& array, int index1, int index2) {
+    if (index1 > index2) {
+        return;
+    }
+    int p = array[(index1 + index2) / 2];
+    int left = index1;
+    int right = index2;
+
+    while (left <= right) {
+        while (array[left] < p) {
+            ++left;
+        }
+        while (array[right] > p) {
+            --right;
+        }
+        if (left <= right) {
+            int tmp = 0;
+            tmp = array[left];
+            array[left] = array[right];
+            array[right] = tmp;
+            ++left;
+            --right;
+        }
+    }
+    q_sort(array, index1, right);
+    q_sort(array, left, index2);
+}
+int main() {
+    const size_t m = 1000000;
+    int n, l, r;
+    std::vector<int> array(m);
+    std::cin >> n;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> array[i];
+    }
+    // array.resize(n);
+
+    l = 0;
+    r = n - 1;
+    q_sort(array, l, r);
+    for (int i = 0; i < n; ++i) {
+        std::cout << array[i] << " ";
+    }
+}
