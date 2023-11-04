19b7995671f7b6db1ca6a3059c60e2e5009213c9
directpi <directpiter1@yandex.ru>
2023-11-05 00:01:54 +0300

    Ya ex5 starosta done

--- starosta3.cpp
+++ starosta3.cpp
@@ -3,29 +3,24 @@
 
 int main() {
     int n;
-    // const int d = 100000;
     std::cin >> n;
     std::vector<int> a(n);
     int sum_arr = 0;
-    int sum = 0;
-    // int tmp = 0;
     for (int i = 0; i < n; ++i) {
         std::cin >> a[i];
         sum_arr += a[i];
     }
-    std::cout << sum_arr << "\n";
-    // a.resize(n);
-    // int sum_l = 0;
-    // int sum_r = 0;
+    int tmp_r = 0;
+    int tmp_l = 0;
     for (int i = 0; i < n; ++i) {
+        int sum = 0;
         int sum_l = 0;
         int sum_r = 0;
-        sum_r = sum_arr - a[i] - (a[i] * (n - 1 - i));
-        std::cout << sum_r << "\n";
-        sum_l += (a[i] * i - sum_l);
-        std::cout << sum_l << "\n";
+        tmp_r += a[i];
+        sum_r = sum_arr - tmp_r - (a[i] * (n - 1 - i));
+        sum_l += (a[i] * i - tmp_l);
+        tmp_l += a[i];
         sum += sum_r + sum_l;
-        std::cout << sum << " "
-                  << "\n";
+        std::cout << sum << " ";
     }
 }
