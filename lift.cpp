fcd8234469857965069fad0689c5981897a550ee
directpi <directpiter1@yandex.ru>
2023-11-05 00:52:00 +0300

    lift 80%

--- lift.cpp
+++ lift.cpp
@@ -6,12 +6,20 @@ int main() {
     std::cin >> k;
     std::cin >> n;
     std::vector<int> array(n);
-    int sum = 0;
+    // int sum = 0;
     for (size_t i = 0; i < n; ++i) {
         std::cin >> array[i];
-        sum += array[i];
+        // sum += array[i];
     }
     int res = 0;
-    res += sum / k * (n + 1);
+    for (int i = n; i > 0; --i) {
+        int tmp = 0;
+        if (array[i] % k != 0) {
+            ++tmp;
+        }
+        res += (array[i] / k + tmp) * i + 1;
+    }
+
+    // res += (sum / k + tmp) * (n + 1);
     std::cout << res;
 }
