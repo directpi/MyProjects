8ff930d0fe93b226d6bc3ddca4683bdb63c2af50
directpi <directpiter1@yandex.ru>
2023-11-16 11:01:30 +0300

    Ya 4 3 A Dijkstra done

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,6 +1,20 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
+int min(vector<long>& dist, vector<bool>& visited, int n) {
+    int k = 1, res = 0;
+    long tmp = __LONG_MAX__;
+    while (k < n + 1) {
+        if (visited[k] == false && dist[k] < tmp) {
+            tmp = dist[k];
+            res = k;
+        }
+        ++k;
+    }
+    // int res = min_element(dist[1], dist[n]);
+    return res;
+}
 int main() {
     int n, s, f;
     cin >> n >> s >> f;
@@ -17,49 +31,40 @@ int main() {
         }
     }
     dist[s] = 0;
-    prev[1] = -1;
+    prev[s] = -1;
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= n; ++j) {
             cin >> matrix[i][j];
         }
     }
-
-    for (int i = s; i <= n; ++i) {
+    // int pov = f - s;
+    for (int i = 1; i <= n; ++i) {
+        int ind = min(dist, visited, n);
+        if (ind == 0) {
+            break;
+        }
         for (int j = 1; j <= n; ++j) {
-            if (visited[i] == false) {
-                if (matrix[i][j] != 0 && matrix[i][j] != (-1)) {
-                    if (dist[j] > matrix[i][j] && j != s) {
-                        dist[j] = matrix[i][j];
-                        prev[j] = i;
+            if (visited[ind] == false) {
+                if (matrix[ind][j] != 0 && matrix[ind][j] != (-1)) {
+                    if (dist[j] > (matrix[ind][j] + dist[ind]) && j != s) {
+                        dist[j] = matrix[ind][j] + dist[ind];
+                        prev[j] = ind;
                     }
                 }
             }
         }
-        visited[i] = true;
+        visited[ind] = true;
     }
-    if (s != 1) {
-        for (int i = 1; i <= s - 1; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                if (visited[i] == false) {
-                    if (i != j && matrix[i][j] != (-1)) {
-                        if (dist[j] > matrix[i][j] && j != s) {
-                            dist[j] = matrix[i][j];
-                            prev[j] = i;
-                        }
-                    }
-                }
-            }
-            visited[i] = true;
-        }
-    }
-    int i = f;
-    int res = 0;
-    while (prev[i] != 0) {
-        if (dist[i] != 0) {
-            res += dist[i];
-            i = prev[i];
-        }
-    }
+    // int i = f;
+    int res = dist[f];
+    // int cnt = 0;
+    // while (dist[i] != 0 || dist[prev[i]] != __LONG_MAX__) {
+    //     if (dist[i] != 0) {
+    //         res += dist[i] - dist[prev[i]];
+    //         i = prev[i];
+    //         ++cnt;
+    //     }
+    // }
     if (s == f) {
         res = 0;
         cout << res;
