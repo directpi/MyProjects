bfde48a4ac299cb00ca7ed0e256fde446e377a49
directpi <directpi@student.21-school.ru>
2025-09-18 21:29:07 +0300

    directpi [remove comments TicTacToeApp]

--- Java_Projects_S21/Tic_Tac_Toe/src/main/java/s21/example/TicTacToeApplication.java
+++ Java_Projects_S21/Tic_Tac_Toe/src/main/java/s21/example/TicTacToeApplication.java
@@ -5,17 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import s21.example.domain.service.AuthService;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-//import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
-//import org.springframework.boot.autoconfigure.websocket.reactive.WebSocketReactiveAutoConfiguration;
-
 @SpringBootApplication(scanBasePackages = "s21.example")
-//@EnableAutoConfiguration(exclude = {
-  //      DataSourceAutoConfiguration.class, // Пример отключения автоконфигурации для DataSource
-    //    WebSocketServletAutoConfiguration.class, // Отключение WebSocket
-      //  WebSocketReactiveAutoConfiguration.class // Отключение реактивного WebSocket
-//})
+
 public class TicTacToeApplication {
 
     public static void main(String[] args) {
